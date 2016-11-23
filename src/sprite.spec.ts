7f04572e7035cd87f76cf5001c28ec5c9a73d3c2
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-11-23 22:30:38 +0300

    Hello, svg2sprite@2.0.0

--- src/sprite.spec.ts
+++ src/sprite.spec.ts
@@ -7,26 +7,23 @@ import * as sprite from './sprite';
 
 describe('Sprite', function() {
 
-  it('Should work', () => {
-    const options = {
-      inline: true,
-      clean: {
-        stripComment: true
-      }
-    };
+	it('Should work', () => {
+		const options = {
+			inline: true
+		};
 
-    return sprite.makeSprite('fixtures', ['sprite.svg'], options)
-      .then((result) => {
-        assert.ok(result.sprite.indexOf('<svg position=') !== -1);
+		return sprite.makeSprite('fixtures', ['sprite.svg'], options)
+			.then((result) => {
+				assert.ok(result.sprite.indexOf('position=') !== -1);
 
-        return result.write('.tmp/sprite.svg');
-      })
-      .then(() => {
-        const expected = fs.readFileSync('fixtures/sprite.svg', 'utf-8');
-        const result = fs.readFileSync('.tmp/sprite.svg', 'utf-8');
+				return result.write('.tmp/sprite.svg');
+			})
+			.then(() => {
+				const expected = fs.readFileSync('fixtures/sprite.svg', 'utf-8');
+				const result = fs.readFileSync('.tmp/sprite.svg', 'utf-8');
 
-        assert.equal(result, expected.replace(/[\r\n\t]|\s{2,}/g, ''));
-      });
-  });
+				assert.equal(result, expected.replace(/[\r\n\t]|\s{2,}/g, ''));
+			});
+	});
 
 });
--- src/sprite.ts
+++ src/sprite.ts
@@ -8,78 +8,78 @@ import * as readdir from 'recursive-readdir';
 import * as svgSprite from 'svg2sprite';
 
 function readdirPromise(dir: string, ignore: string[]): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    readdir(dir, ignore, (err, files) => {
-      if (err) {
-        reject(err);
-      }
+	return new Promise((resolve, reject) => {
+		readdir(dir, ignore, (err, files) => {
+			if (err) {
+				return reject(err);
+			}
 
-      resolve(files);
-    });
-  });
+			resolve(files);
+		});
+	});
 }
 
 function readFilePromise(filepath: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filepath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
+	return new Promise((resolve, reject) => {
+		fs.readFile(filepath, 'utf-8', (err, data) => {
+			if (err) {
+				return reject(err);
+			}
 
-      resolve(data);
-    });
-  });
+			resolve(data);
+		});
+	});
 }
 
 function writeFilePromise(filepath: string, data: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    mkdirp(path.dirname(filepath), (mkdirpError) => {
-      if (mkdirpError) {
-        reject(mkdirpError);
-      }
+	return new Promise((resolve, reject) => {
+		mkdirp(path.dirname(filepath), (mkdirpError) => {
+			if (mkdirpError) {
+				return reject(mkdirpError);
+			}
 
-      fs.writeFile(filepath, data, (writeError, files) => {
-        if (writeError) {
-          reject(writeError);
-        }
+			fs.writeFile(filepath, data, (writeError, files) => {
+				if (writeError) {
+					return reject(writeError);
+				}
 
-        resolve(files);
-      });
-    });
-  });
+				resolve(files);
+			});
+		});
+	});
 }
 
 export interface IResult {
-  /**
-   * Sprite data.
-   */
-  sprite: string;
-  /**
-   * A feature that allows you to write the resulting sprite to disk.
-   */
-  write: (filepath: string) => void;
+	/**
+	 * Sprite data.
+	 */
+	sprite: string;
+	/**
+	 * A feature that allows you to write the resulting sprite to disk.
+	 */
+	write: (filepath: string) => void;
 }
 
 export function makeSprite(sourceDir: string, ignore?: string[], options?: IOptions): Promise<IResult> {
-  ignore = ignore.concat(['!*.svg']);
+	ignore = ignore.concat(['!*.svg']);
 
-  const sprite = svgSprite.collection(options);
+	const sprite = svgSprite.collection(options);
 
-  return readdirPromise(sourceDir, ignore).then((files) => {
-    const promises = files.map((filename) => {
-      return readFilePromise(filename).then((content) => {
-        const name = path.basename(filename, '.svg');
-        sprite.add(name, content);
-      });
-    });
+	return readdirPromise(sourceDir, ignore).then((files) => {
+		const promises = files.map((filename) => {
+			return readFilePromise(filename).then((content) => {
+				const name = path.basename(filename, '.svg');
+				sprite.add(name, content);
+			});
+		});
 
-    return Promise.all(promises);
-  }).then(() => {
-    const result = sprite.compile();
+		return Promise.all(promises);
+	}).then(() => {
+		const result = sprite.compile();
 
-    return {
-      sprite: sprite.compile(),
-      write: (filepath: string) => writeFilePromise(filepath, result)
-    };
-  });
+		return {
+			sprite: sprite.compile(),
+			write: (filepath: string) => writeFilePromise(filepath, result)
+		};
+	});
 }
